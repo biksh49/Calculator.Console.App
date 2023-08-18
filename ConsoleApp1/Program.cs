863c669266b6d181f9f86784ef5266923f42d062
Biki Shah <biki.shah@avishkaram.com>
2023-08-18 08:44:18 +0545

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+
+            string isUserInput = Console.ReadLine();
+
+            if (isUserInput == "Yes")
+            {
+                int firstInput =int.Parse(Console.ReadLine());
+                int secondInput = int.Parse(Console.ReadLine());
+
+                int.Parse("Yes");
+                int sum=firstInput+ secondInput;
+                Console.WriteLine($"The output is :{sum}");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Have a nice day!!");
+            }
+
+            Addition(5,6);
+            
+           
+            
+        }
+        static int Addition(int firstInput,int secondInput)
+        {
+            int sum = firstInput + secondInput;
+            return sum;
+        }
+        static int Multiplication(int firstInput, int secondInput)
+        {
+            int product = firstInput * secondInput;
+            return product;
+        }
+        static int Subtraction(int firstInput, int secondInput)
+        {
+            int subtract = firstInput-secondInput;
+            return subtract;
+        }
+        static int Division(int firstInput,int secondInput)
+        {
+            double divide = firstInput/secondInput;
+            return divide;
+        }
+        
+    }
+}
